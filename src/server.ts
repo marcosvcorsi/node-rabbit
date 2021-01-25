2085514a60a6ef8e979af115e3bd4561582ccd9d
Marcos Corsi <marcosvcorsi@gmail.com>
2021-01-24 22:23:22 -0300

    chore: clear structure

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
 
 app.listen(process.env.PORT || 3333, () => console.log('Server is running'))
